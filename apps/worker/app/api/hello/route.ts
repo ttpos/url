a3c8eabf1fb0c2ad1fce4c4f1763404a991d722c
Roy <62500486+aazf@users.noreply.github.com>
2024-08-16 01:05:35 +0800

    Fix lint.

--- apps/worker/app/api/hello/route.ts
+++ apps/worker/app/api/hello/route.ts
@@ -1,7 +1,7 @@
-import type { NextRequest } from 'next/server'
-import { getRequestContext } from '@cloudflare/next-on-pages'
+import type { NextRequest } from "next/server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
   const context = getRequestContext();
