7bee3851cd1b490c5a6176e770b9415b8bc64a23
Roy <62500486+aazf@users.noreply.github.com>
2024-08-28 03:34:41 +0800

    fix database schema

---
+++ apps/worker/database/0000_keen_shinko_yamashiro.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `links` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`domain` text NOT NULL,
+	`short_code` text NOT NULL,
+	`url` text NOT NULL,
+	`user_id` integer NOT NULL,
+	`expires_at` integer,
+	`is_delete` integer DEFAULT 0
+);
+--> statement-breakpoint
+CREATE TABLE `pages` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`domain` text NOT NULL,
+	`short_code` text NOT NULL,
+	`user_id` text NOT NULL,
+	`template` text NOT NULL,
+	`data` blob NOT NULL,
+	`is_delete` integer DEFAULT 0,
+	`expires_at` integer
+);
+--> statement-breakpoint
+CREATE INDEX `links_domain` ON `links` (`domain`);--> statement-breakpoint
+CREATE INDEX `links_short_code` ON `links` (`short_code`);--> statement-breakpoint
+CREATE INDEX `pages_domain` ON `pages` (`domain`);--> statement-breakpoint
+CREATE INDEX `pages_short_code` ON `pages` (`short_code`);
