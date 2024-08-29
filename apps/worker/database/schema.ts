ec0c9ceda876233c8aa15a3e6ef77db837e11ba4
cd996 <cdhj996@gmail.com>
2024-08-29 20:55:13 +0800

    refactor(database): Revise the links and pages tables to enhance uniqueness.
    - Generate unique link and page entries based on hash values derived from user IDs and URL or template data.
    - Remove the unique indexes on short codes and domain names; now ensure uniqueness in the links and pages tables using hash values.
    - Change the `userId` field from integer to text type to increase flexibility.
    - Update indexes and unique constraints to reflect the new hash-based uniqueness.

--- apps/worker/database/schema.ts
+++ apps/worker/database/schema.ts
@@ -1,34 +1,28 @@
-import { blob, index, integer, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core'
+import { blob, integer, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core'
 
 export const links = sqliteTable('links', {
   id: integer('id').primaryKey({ autoIncrement: true }).notNull(),
-  domain: text('domain').notNull(),
-  shortCode: text('short_code').notNull(),
   url: text('url').notNull(),
-  userId: integer('user_id').notNull(),
+  userId: text('user_id').notNull(),
   expiresAt: integer('expires_at'),
   isDelete: integer('is_delete').default(0),
+  hash: text('hash').notNull(),
 }, (table) => {
   return {
-    domain: index('links_domain').on(table.domain),
-    shortCode: index('links_short_code').on(table.shortCode),
-    uniqueId: uniqueIndex('links_unique_id').on(table.domain, table.shortCode),
+    sha1: uniqueIndex('links_hash').on(table.hash),
   }
 })
 
 export const pages = sqliteTable('pages', {
   id: integer('id').primaryKey({ autoIncrement: true }),
-  domain: text('domain').notNull(),
-  shortCode: text('short_code').notNull(),
   userId: text('user_id').notNull(),
   template: text('template').notNull(),
   data: blob('data').notNull(),
   isDelete: integer('is_delete').default(0),
   expiresAt: integer('expires_at'),
+  hash: text('hash').notNull(),
 }, (table) => {
   return {
-    domain: index('pages_domain').on(table.domain),
-    shortCode: index('pages_short_code').on(table.shortCode),
-    uniqueId: uniqueIndex('pages_unique_id').on(table.domain, table.shortCode),
+    sha1: uniqueIndex('pages_hash').on(table.hash),
   }
 })
