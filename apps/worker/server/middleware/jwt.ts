df6f60b970ac3d3e507e495c7b3adc86bd9239fa
cd996 <cdhj996@gmail.com>
2024-08-30 18:15:10 +0800

    feat(server): Integrate JWT Authentication
    - Integrated JWT authentication into the server middleware to ensure the security of API requests.
    - Updated environment configurations and Nuxt settings accordingly to support the new functionality.

---
+++ apps/worker/server/middleware/jwt.ts
@@ -0,0 +1,45 @@
+import { defineEventHandler } from 'h3'
+import { jwtVerifyFn } from '@@/server/utils/jwtVerifier'
+
+export default defineEventHandler(async (event) => {
+  const url = event.node.req.url
+
+  if (url?.startsWith('/api')) {
+    const { jwtPubkey } = useRuntimeConfig(event)
+
+    if (!jwtPubkey) {
+      event.node.res.statusCode = 500
+      return {
+        code: 500,
+        message: 'JWT public key not found',
+      }
+    }
+
+    const authorizationHeader = event.node.req.headers.authorization
+
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+      event.node.res.statusCode = 401
+      return {
+        code: 401,
+        message: 'Unauthorized',
+      }
+    }
+
+    try {
+      const token = authorizationHeader.substring(7)
+      const verify = await jwtVerifyFn(jwtPubkey, token)
+
+      logger.info('JWT verification succeeded', verify)
+
+      event.context.auth = verify.payload
+    }
+    catch (error) {
+      logger.error('JWT verification failed', error)
+      event.node.res.statusCode = 401
+      return {
+        code: 401,
+        message: 'Invalid token',
+      }
+    }
+  }
+})
