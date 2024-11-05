8e59ca81d60ec5fc88dc524a80ae82d655d56029
cd <cdhj996@gmail.com>
2024-11-05 19:07:29 +0800

    feat(official-website): add about introduction page

---
+++ apps/official-website/app/pages/about.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <UContainer>
+      <UBlogPost
+        title="团队介绍"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum"
+        orientation="vertical"
+        :image="{ src: 'https://picsum.photos/640/360', alt: 'Nuxt 3.9' }"
+        class="py-16"
+      >
+        <div class="text-gray-900 dark:text-white text-xl font-semibold truncate group-hover:text-gray-600 dark:group-hover:text-gray-300 transition-colors duration-200 mt-4">
+          联系我们
+        </div>
+        <template #authors>
+          <UIcon name="material-symbols:attach-email-outline" class="w-5 h-5" />
+        </template>
+        <template #date>
+          <a href="mailto:example@tinylink.com" class="hover:text-primary-500 transition-colors">
+            example@tinylink.com
+          </a>
+        </template>
+      </UBlogPost>
+    </UContainer>
+  </div>
+</template>
